d1ddd07d22415424a95e6a92139fd0544d1741b8
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2022-10-10 09:06:20 +0700

    Update UI

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(color: Colors.black),
-          centerTitle: true,
-          titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
-        ),
+            // backgroundColor: Colors.white,
+            // iconTheme: IconThemeData(color: Colors.black),
+            // centerTitle: true,
+            // titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
+            ),
       ),
       initialRoute: '/',
       routes: {
